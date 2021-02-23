e47bf200d63010684a09acd7dbbd1a98e9845390
Jason Sackey <planetearthjason@gmail.com>
2021-02-23 19:14:51 +0000

    *

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import { init as dragSystemInit, makeDraggable } from "/src/app/drag.js";
-import { init as initCard } from "/src/app/card.js";
+import { init as dragSystemInit, makeDraggable } from "./app/drag.js";
+import { init as initCard } from "./app/card.js";
+
+var zoomLevel = 0;
 
 function init() {
   dragSystemInit(document);
   document.querySelectorAll(".card").forEach(initCard);
   makeDraggable(document.querySelector(".main-view"));
+  document.addEventListener("keydown", (event) => {
+    console.log("event: ", event);
+    if (event.key === "z") {
+      zoom(1);
+    } else if (event.key === "x") {
+      zoom(-1);
+    }
+  });
+}
+
+function zoom(amount) {
+  zoomLevel += amount;
 }
 
 init();
